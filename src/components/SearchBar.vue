7c67d48f3247ca4b830864c8331f5f1f1ba8dd70
Ronny Tite <ronny.tite@m-itrust.com>
2023-02-20 17:14:26 +0100

    implement search bar on home

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <IonSearchbar
+    :search-icon="searchCircle"
+    :animated="true"
+    placeholder="Search Here"
+    :debounce="500"
+    @ionChange="$event.detail.value ? $emit('on-search',$event.detail) : () => {}"
+    @ionClear="$emit('clear',$event)"
+    ></IonSearchbar>
+  </template>
+
+<script lang="ts">
+import { IonSearchbar } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { searchCircle } from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'SearchBar',
+  components: { IonSearchbar },
+  setup() {
+    return { searchCircle };
+  },
+});
+</script>
