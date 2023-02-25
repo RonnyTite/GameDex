feb43cfc49e12085d90345f6cd3c9989886be461
Ronny <tite.ronny@gmail.com>
2023-02-25 19:06:53 +0100

    adjust API (#18)

--- src/scripts/RequestManager.ts
+++ src/scripts/RequestManager.ts
@@ -1,6 +1,11 @@
-import axios from 'axios';
-
+import axios, { AxiosPromise } from 'axios';
 // export const BASE_URL = 'http://192.168.86.33:1337'; // RasberryPI or server
 export const BASE_URL = 'http://localhost:1337'; // Working in localhost
 
-export default axios.create();
+export const axiosInstance = axios.create();
+
+export default {
+  get<T>(url:string):AxiosPromise<T> {
+    return axiosInstance.get(url);
+  },
+};
