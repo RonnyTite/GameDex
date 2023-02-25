c2293eaabc210218929635e42b29a6df4a427690
Ronny Tite <ronny.tite@m-itrust.com>
2023-02-25 19:03:05 +0100

    adjust API

---
+++ tests/unit/scripts/GiantBombApi.spec.ts
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import sinon, { SinonStub } from 'sinon';
+import GiantBombApi from '../../../src/scripts/GiantBombApi';
+import { axiosInstance } from '../../../src/scripts/RequestManager';
+
+describe('Api routes', () => {
+  let axiosMock:SinonStub;
+  beforeEach(() => {
+    axiosMock = sinon.stub(axiosInstance, 'get').resolves({});
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+  it('check URL request for search', () => GiantBombApi.makeSearch('bayo')
+    .then(() => {
+      // sinon.assert.calledOnceWithExactly(axiosMock, `${BASE_URL}/search/bayo`);
+      const url = axiosMock.getCall(0).args[0];
+      expect(url).toMatch('/search/bayo');
+    }));
+
+  it('check URL request for game profile', () => GiantBombApi.fetchGameProfile('12404')
+    .then(() => {
+      const url = axiosMock.getCall(0).args[0];
+      expect(url).toMatch('/game/12404');
+    }));
+});
