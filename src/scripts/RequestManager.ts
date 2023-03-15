f990fa35a6b16a770cf02e95bf58dd55d1b28c82
Ronny <tite.ronny@gmail.com>
2023-03-15 13:12:31 +0100

    add https to server url (#29)

--- src/scripts/RequestManager.ts
+++ src/scripts/RequestManager.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosPromise } from 'axios';
-// export const BASE_URL = 'http://192.168.86.33:1337'; // RasberryPI or server
-export const BASE_URL = 'http://localhost:1337'; // Working in localhost
+// export const BASE_URL = 'https://192.168.86.33:1337'; // RasberryPI or server
+export const BASE_URL = 'https://localhost:1337'; // Working in localhost
 
 export const axiosInstance = axios.create();
 
